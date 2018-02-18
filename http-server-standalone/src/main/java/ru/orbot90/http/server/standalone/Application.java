9848fdaa64896105332941b7c7f490d011c0d298
Orbot90 <orbot90@gmail.com>
2018-02-18 12:05:28 +0300

    Slightly changed error handling

--- http-server-standalone/src/main/java/ru/orbot90/http/server/standalone/Application.java
+++ http-server-standalone/src/main/java/ru/orbot90/http/server/standalone/Application.java
@@ -15,20 +15,21 @@ public class Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         LOGGER.info("Starting application");
-        Properties properties = new Properties();
-        properties.load(new InputStreamReader(Application.class.getClassLoader().getResourceAsStream("server-config.properties")));
-        int threadPoolSize = Integer.valueOf(properties.getProperty("server.threadpool.size"));
-        int serverPort = Integer.valueOf(properties.getProperty("server.port"));
 
         try {
+            Properties properties = new Properties();
+            properties.load(new InputStreamReader(Application.class.getClassLoader().getResourceAsStream("server-config.properties")));
+            int threadPoolSize = Integer.valueOf(properties.getProperty("server.threadpool.size"));
+            int serverPort = Integer.valueOf(properties.getProperty("server.port"));
             new ServerRunner().threadPoolSize(threadPoolSize)
                     .serverPort(serverPort)
                     .requestHandler(new TemporaryTestRequestHandler())
                     .run();
         } catch (Throwable e) {
             LOGGER.error("Error running server. Application is stopped", e);
+            System.exit(1);
         }
     }
 }
